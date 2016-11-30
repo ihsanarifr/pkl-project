a5c7b0e70a5667db8dd549309d1603b7af6cb027
Ihsan Arif Rahman <ihsan_arif@apps.ipb.ac.id>
2016-11-30 13:30:16 +0700

    tambah record database

--- application/views/data_siswa/view.php
+++ application/views/data_siswa/view.php
@@ -17,33 +17,33 @@
                         <div class="col-md-8">
                             <table class="table table-striped">
                                 <tr>
-                                    <td width="100">Nama</td>
-                                    <td>:<td>
+                                    <td width="200">Nama</td>
+                                    <td width="30">:</td>
                                     <td>Nama Aslina</td>
                                 </tr>
                                 <tr>
                                     <td width="100">Nama</td>
-                                    <td>:<td>
+                                    <td>:</td>
                                     <td>Nama Aslina</td>
                                 </tr>
                                 <tr>
                                     <td width="100">Nama</td>
-                                    <td>:<td>
+                                    <td>:</td>
                                     <td>Nama Aslina</td>
                                 </tr>
                                 <tr>
                                     <td width="100">Nama</td>
-                                    <td>:<td>
+                                    <td>:</td>
                                     <td>Nama Aslina</td>
                                 </tr>
                                 <tr>
                                     <td width="100">Nama</td>
-                                    <td>:<td>
+                                    <td>:</td>
                                     <td>Nama Aslina</td>
                                 </tr>
                                 <tr>
                                     <td width="100">Nama</td>
-                                    <td>:<td>
+                                    <td>:</td>
                                     <td>Nama Aslina</td>
                                 </tr>
                             </table>                            
