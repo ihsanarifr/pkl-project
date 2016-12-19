49c9026e487ebbe492cae294266fbc994b932aa4
vinimulyani <vinimulyani46@gmail.com>
2016-12-19 10:03:26 +0700

    menghapus upload

--- application/views/data_siswa/create.php
+++ application/views/data_siswa/create.php
@@ -29,12 +29,6 @@
                         <label for="exampleInputPassword1">Password</label>
                         <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                     </div>
-                    <div class="form-group">
-                        <label for="exampleInputFile">File input</label>
-                        <input type="file" id="exampleInputFile">
-                        <p class="help-block">Example block-level help text here.</p>
-                    </div>
-                    
                     </form>
                 </div>
             </div>
