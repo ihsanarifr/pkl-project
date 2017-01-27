056cd4ae349330838d3821f919a8fef4608be6f1
dina <dinadewianjani19@gmail.com>
2017-01-27 16:16:22 +0700

    tstt

--- application/controllers/Aspek_penilaian.php
+++ application/controllers/Aspek_penilaian.php
@@ -17,6 +17,10 @@ class Aspek_penilaian extends CI_Controller
 		$data['main']='aspek_penilaian/index';
 		$data['menu']=1;
 		$data['judul']='Data Aspek Penilaian';
+
+        $data['aspek_penilaian'] = $this->aspek_penilaian_model->viewall()->result();
+
+
 		$data['css']=array('css/datatables.min');
         $data['js']= array('js/jquery.dataTables','js/dataTables.bootstrap');
 		$this->load->view('layouts/master',$data);
@@ -26,6 +30,7 @@ class Aspek_penilaian extends CI_Controller
 
     public function add()
     {
+
         $data['main']='aspek_penilaian/create';
 		$data['menu']=1;
 		$data['judul']='Tambah Aspek Penilaian';
@@ -34,7 +39,31 @@ class Aspek_penilaian extends CI_Controller
 
     public function save()
     {
+        $this->form_validation->set_rules('nama', 'Nama Aspek Penilaian', 'required');
 
+        $data = array(
+            'name' => $this->input->post('nama'),
+        };
+
+        if ($this->form_validation->run() == FALSE)
+        {
+            $data['main']='aspek_penilaian/create';
+            $data['menu']=1;
+            $data['judul']='Tambah Aspek Penilaian';
+            
+            $this->session->set_flashdata('status','danger');
+            $this->session->set_flashdata('message', validation_errors());
+
+            $this->load->view('layouts/master',$data);
+        }
+        else
+        {
+            // memanggil fungsi di model grup_user_model
+            $this->grup_user_model->save($data);
+            $this->session->set_flashdata('status','success');
+            $this->session->set_flashdata('message', 'Aspek penilaian sudah selesai');
+            redirect('aspek_penilaian');
+        }
     }
 
     public function edit($id)
@@ -47,19 +76,50 @@ class Aspek_penilaian extends CI_Controller
         $data['main']='aspek_penilaian/edit';
 		$data['menu']=1;
 		$data['judul']='Edit Aspek Penilaian';
+
+        $data['aspek_penilaian'] = $this->aspek_penilaian_model->select_by_id($id)->row();
 		$this->load->view('layouts/master',$data);
 
     }
 
     public function update()
     {
+         $this->form_validation->set_rules('nama', 'Aspek Penilaian', 'required');
 
+        $data = array(
+            'id' => $this->input->post('id'),
+            'name' => $this->input->post('nama'),
+        );
+
+        if ($this->form_validation->run() == FALSE)
+        {
+            $this->session->set_flashdata('status','danger');
+            $this->session->set_flashdata('message', validation_errors());
+
+            return $this->edit($data['id']);
+        }
+        else
+        {
+            $this->grup_user_model->update($data);
+            $this->session->set_flashdata('status','success');
+            $this->session->set_flashdata('message', 'Ubah aspek penilaian sudah selesai');
+            redirect('aspek_penilaian');
+        }
     }
 
     public function delete($id)
     {
         if(empty($id))
         {
+            $this->session->set_flashdata('status','danger');
+            $this->session->set_flashdata('message', 'Anda Tidak bisa akses');
+            redirect('aspek_penilaian');
+        }
+        else
+        {
+            $this->aspek_penilaian->delete($id);
+            $this->session->set_flashdata('status','success');
+            $this->session->set_flashdata('message', 'Hapus aspek penilaian sudah selesai');
             redirect('home');
         }
     }
--- application/models/aspek_penilaian_model.php
+++ application/models/aspek_penilaian_model.php
@@ -2,5 +2,32 @@
 
 class Aspek_penilaian_model extends CI_Model
 {
+	 public function viewall()
+    {
+        return $this->db->get('Aspek_penilaian_model');
+    }
+
+    public function select_by_id($id)
+    {
+        $this->db->where('Aspek_penilaian_model.id',$id);
+        return $this->db->get('groups');
+    }
+
+    public function save($data)
+    {
+        $this->db->insert('Aspek_penilaian_model',$data);
+    }
+
+    public function update($data)
+    {
+        $id = $data['id'];
+        $this->db->where('id',$id);
+        $this->db->update('Aspek_penilaian_model',$data);
+    }
+
+    public function delete($id)
+    {
+        $this->db->delete('Aspek_penilaian_model',array('id'=>$id));
+    }
     
 }
