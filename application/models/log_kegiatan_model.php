b8bd30e88f3403f9788dc703f61619d7c58e7f12
Ihsan Arif Rahman <ihsan.android@gmail.com>
2017-01-30 14:17:56 +0700

    add function get_by_prakerin_id

--- application/models/log_kegiatan_model.php
+++ application/models/log_kegiatan_model.php
@@ -2,5 +2,8 @@
 
 class Log_kegiatan_model extends CI_Model
 {
-    
+    public function get_by_prakerin_id($id)
+    {
+        return $this->db->where('prakerin_siswa_id',$id)->get('kegiatan')->result();
+    }
 }
