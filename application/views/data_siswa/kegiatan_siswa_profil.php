c699958e6946220bc4c0a5a66355e39d694756d0
vinimulyani <vinimulyani46@gmail.com>
2017-01-30 14:22:12 +0700

    test

--- application/views/data_siswa/kegiatan_siswa_profil.php
+++ application/views/data_siswa/kegiatan_siswa_profil.php
@@ -3,22 +3,22 @@
         <tr>
             <td width="200">Nomor Induk Siswa</td>
             <td width="30">:</td>
-            <td></td>
+            <td><?php echo $siswa->nomor_induk?></td>
         </tr>
         <tr>           
             <td width="100">Nama Lengkap</td>
             <td>:</td>
-            <td></td>
+            <td><?php echo $siswa->nama?></td>
         </tr>
         <tr>
             <td width="100">Tempat, tanggal lahir</td>
             <td>:</td>
-            <td></td>
+             <td><?php echo $siswa->tempat_lahir?>, <?php echo $siswa->tanggal_lahir ?></td>
         </tr>
         <tr>
             <td width="100">Ayah/Ibu</td>
             <td>:</td>
-            <td></td>
+           <td><?php echo $siswa->ayah?>/<?php echo $siswa->ibu ?></td>
         </tr>
     </table>                            
 </div>
