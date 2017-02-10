fa69e92f563bbf7adac0d48e6d96b831690fd856
Ihsan Arif Rahman <ihsan.android@gmail.com>
2017-02-10 23:38:06 +0700

    create helper date indonesia

---
+++ application/helpers/Date_indonesia_helper.php
@@ -0,0 +1,28 @@
+<?php
+
+function TanggalIndo($date)
+{
+	$BulanIndo = array("Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember");
+ 
+	$tahun = substr($date, 0, 4);
+	$bulan = substr($date, 5, 2);
+	$tgl   = substr($date, 8, 2);
+ 
+	$result = $tgl . " " . $BulanIndo[(int)$bulan-1] . " ". $tahun;		
+	return($result);
+}
+
+function DayIndonesia($tanggal)
+{
+	$day = date('D', strtotime($tanggal));	
+	$dayList = array(
+		'Sun' => 'Minggu',
+		'Mon' => 'Senin',
+		'Tue' => 'Selasa',
+		'Wed' => 'Rabu',
+		'Thu' => 'Kamis',
+		'Fri' => 'Jumat',
+		'Sat' => 'Sabtu'
+	);
+	return $dayList[$day];
+}
